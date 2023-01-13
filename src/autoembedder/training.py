ef265e9d36f54a5ab804f72f711eb9e7cb8dc23d
Chris Lemke <11752694+chrislemke@users.noreply.github.com>
2023-01-13 07:28:39 +0100

    maintain: add more pre-commit hooks

--- src/autoembedder/training.py
+++ src/autoembedder/training.py
@@ -14,6 +14,72 @@ from autoembedder.model import Autoembedder, embedded_sizes_and_dims, num_cont_c
 app = typer.Typer(rich_markup_mode="rich")
 
 
+def _check_for_consistent_cat_rows(
+    df: pd.DataFrame, cat_columns: Iterable[List[str]]
+) -> None:
+
+    """Checks if the categorical rows in the the dataframe (`df`) are
+    consistent with the categorical columns (`cat_columns`). This is needed so
+    the is set up correctly. Please check the `--cat_columns` parameter for
+    more information.
+
+    Args:
+        df (pandas.DataFrame): The dataframe containing the categorical columns.
+        cat_columns (List[List[str]]): A list of lists representing the categorical columns which were encoded using the same encoder. E.g.: [['a', 'b'], ['c']]. Check the `--cat_columns` parameter for more information.
+
+    Returns:
+        None
+    """
+
+    df_columns = df.select_dtypes(include="category").columns.to_list()
+    cat_columns = list(itertools.chain(*cat_columns))  # type: ignore
+    assert set(df_columns) == set(
+        cat_columns
+    ), f"""
+        The rows from the dataframe should be consistent to the ones defined in `--cat_columns`! Remember to adjust the
+        `--cat_columns` according to the dataframe.
+        `df_columns` = {df_columns}
+        vs.
+        `cat_columns` = {cat_columns}
+        """
+
+
+def _prepare_and_fit(parameters: Dict, model_params: Dict) -> None:
+    """Prepares the data by creating a training and testing `dataloader`.
+    `num_continuous_cols` is determined and passed to the `Autoembedder` model
+    for creating the needed layers.
+
+    Args:
+        parameters (Dict): Dictionary containing the parameters for the training, and creating the `dataloaders`.
+        model_params (Dict): Dictionary containing the model parameters.
+
+    Returns:
+        None
+    """
+
+    if (
+        torch.backends.mps.is_available() is False
+        or parameters.get("use_mps", False) is False
+    ):
+        torch.set_default_tensor_type(torch.DoubleTensor)
+
+    train_dl = dataloader(parameters["train_input_path"], parameters)
+    test_dl = dataloader(parameters["test_input_path"], parameters)
+    num_continuous_cols = num_cont_columns(train_dl.dataset.ddf)
+    if parameters.get("drop_cat_columns", False) is False:
+        _check_for_consistent_cat_rows(
+            train_dl.dataset.ddf, ast.literal_eval(parameters["cat_columns"])
+        )
+    embedded_sizes = embedded_sizes_and_dims(
+        train_dl.dataset.ddf,
+        test_dl.dataset.ddf,
+        ast.literal_eval(parameters.get("cat_columns", "[]")),
+    )
+    model = Autoembedder(model_params, num_continuous_cols, embedded_sizes)
+
+    learner.fit(parameters, model, train_dl, test_dl)
+
+
 @app.command(help="Runs the training process for the autoembedder.")  # type: ignore
 def main(
     batch_size: int = typer.Option(32, min=1, help="Batch size for the training."),
@@ -84,13 +150,14 @@ def main(
         """,
     ),
 ) -> None:
-    """
-    Main function for parsing arguments and start `_prepare_and_fit`.
-    In the [documentation](https://chrislemke.github.io/autoembedder/#parameters) all possible parameters are listed.
+    """Main function for parsing arguments and start `_prepare_and_fit`. In
+    the.
+
+    [documentation](https://chrislemke.github.io/autoembedder/#parameters) all
+    possible parameters are listed.
 
     Returns:
         None
-
     """
 
     cat_columns = cat_columns.replace("\\", "")
@@ -129,72 +196,6 @@ def main(
     }
 
     _prepare_and_fit(parameters, m_config)
-
-
-def _prepare_and_fit(parameters: Dict, model_params: Dict) -> None:
-
-    """
-    Prepares the data by creating a training and testing `dataloader`. `num_continuous_cols` is determined and passed to the
-    `Autoembedder` model for creating the needed layers.
-
-    Args:
-        parameters (Dict): Dictionary containing the parameters for the training, and creating the `dataloaders`.
-        model_params (Dict): Dictionary containing the model parameters.
-
-    Returns:
-        None
-    """
-
-    if (
-        torch.backends.mps.is_available() is False
-        or parameters.get("use_mps", False) is False
-    ):
-        torch.set_default_tensor_type(torch.DoubleTensor)
-
-    train_dl = dataloader(parameters["train_input_path"], parameters)
-    test_dl = dataloader(parameters["test_input_path"], parameters)
-    num_continuous_cols = num_cont_columns(train_dl.dataset.ddf)
-    if parameters.get("drop_cat_columns", False) is False:
-        _check_for_consistent_cat_rows(
-            train_dl.dataset.ddf, ast.literal_eval(parameters["cat_columns"])
-        )
-    embedded_sizes = embedded_sizes_and_dims(
-        train_dl.dataset.ddf,
-        test_dl.dataset.ddf,
-        ast.literal_eval(parameters.get("cat_columns", "[]")),
-    )
-    model = Autoembedder(model_params, num_continuous_cols, embedded_sizes)
-
-    learner.fit(parameters, model, train_dl, test_dl)
-
-
-def _check_for_consistent_cat_rows(
-    df: pd.DataFrame, cat_columns: Iterable[List[str]]
-) -> None:
-
-    """
-    Checks if the categorical rows in the the dataframe (`df`) are consistent with the categorical columns (`cat_columns`).
-    This is needed so the is set up correctly. Please check the `--cat_columns` parameter for more information.
-
-    Args:
-        df (pandas.DataFrame): The dataframe containing the categorical columns.
-        cat_columns (List[List[str]]): A list of lists representing the categorical columns which were encoded using the same encoder. E.g.: [['a', 'b'], ['c']]. Check the `--cat_columns` parameter for more information.
-
-    Returns:
-        None
-    """
-
-    df_columns = df.select_dtypes(include="category").columns.to_list()
-    cat_columns = list(itertools.chain(*cat_columns))  # type: ignore
-    assert set(df_columns) == set(
-        cat_columns
-    ), f"""
-        The rows from the dataframe should be consistent to the ones defined in `--cat_columns`! Remember to adjust the
-        `--cat_columns` according to the dataframe.
-        `df_columns` = {df_columns}
-        vs.
-        `cat_columns` = {cat_columns}
-        """
 
 
 if __name__ == "__main__":
